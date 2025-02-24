107f30975742fba0e7572c13c11aaae7312ea6ef
Teemu Andersson <tjvander@gmail.com>
2025-02-24 18:58:01 +0200

    fix tests to be independent

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,17 @@
-import { render, screen, fireEvent } from '@testing-library/react';
-import { describe, it, expect, beforeEach, vi } from 'vitest';
+import { render, screen, fireEvent, cleanup } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
 import App from './App';
 
 describe('ToDo App', () => {
 
-
+  beforeEach(() => {
+    localStorage.clear(); 
+  });
 
   it('Adds a New Item', () => {
     render(<App />);
+
+    expect(screen.queryByText('NewItem')).not.toBeInTheDocument();
 
     const input = screen.getByPlaceholderText('Add To Do');
     const addButton = screen.getByText('Add');
@@ -15,14 +19,21 @@ describe('ToDo App', () => {
     fireEvent.change(input, { target: { value: 'NewItem' } });
     fireEvent.click(addButton);
 
-    expect(screen.getByText('NewItem')).toBeInTheDocument();
+    expect(screen.queryByText('NewItem')).toBeInTheDocument();
   });
 
   it('Deletes an Item', () => {
     render(<App />);
 
+    expect(screen.queryByText('NewItem')).not.toBeInTheDocument();
 
-    expect(screen.getByText('NewItem')).toBeInTheDocument();
+    const input = screen.getByPlaceholderText('Add To Do');
+    const addButton = screen.getByText('Add');
+
+    fireEvent.change(input, { target: { value: 'NewItem' } });
+    fireEvent.click(addButton);
+
+    expect(screen.queryByText('NewItem')).toBeInTheDocument();
 
     const deleteButton = screen.getByText('Delete');
     fireEvent.click(deleteButton);
